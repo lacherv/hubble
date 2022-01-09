4269aa8e12a7d1199791afe5d9be0d2ed4a7d2bd
Christian Awelakoue <awelakoue@gmail.com>
2022-01-09 09:58:54 +0200

    'Nesting'

--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -3,4 +3,13 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
     background-color: #ebfbff;
     padding: 40px 0;
+
+    // Nesting
+    h1 {
+        color: red;
+    }
+
+    &:hover {
+        background-color: black;
+    }
 `;
