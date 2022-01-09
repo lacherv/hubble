791f7dcdc99b48c9649d39db9cbd308fbf51b43a
Christian Awelakoue <awelakoue@gmail.com>
2022-01-09 10:11:28 +0200

    theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { Container } from "./components/styles/Container.styled";
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+};
+
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Container>
-        <h1>Hello There !</h1>
-      </Container>
-    </>
- 
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>Hello There !</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
