0a41180a4f1bcc6fa05b3b12cca34cebdc7b5ecf
Christian Awelakoue <awelakoue@gmail.com>
2022-01-09 11:06:44 +0200

    create the flex styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Container } from "./styles/Container.styled";
 import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
 import { Button } from "./styles/Button.styled";
+import { Flex } from "./styles/Flex.styled";
 
 const Header = () => {
     return (
@@ -10,6 +11,17 @@ const Header = () => {
                     <Logo src='./images/logo.svg' alt='' />
                     <Button>Try it Free</Button>
                 </Nav>
+
+                <Flex>
+                    <div>
+                        <h1>Build The Comminuty Your Fans Will Love</h1>
+                        <p>
+                            Huddle re-imagines the way we build communities. You have a voice, but 
+                            so does your audience. Create connections with your users as ypou engage in genuine
+                            discussion.
+                        </p>
+                    </div>
+                </Flex>
             </Container>
         </StyledHeader>
     );
--- src/components/styles/Button.styled.jsx
+++ src/components/styles/Button.styled.jsx
@@ -8,4 +8,12 @@ export const Button = styled.button`
     font-size: 16px;
     font-weight: 700;
     padding: 15px 60px;
+    background-color: ${({ bg }) => bg || '#fff'};
+    color: ${({ color }) => color || '#333'};
+
+    &:hover {
+        opacity: 0.9;
+        transform: scale(0.98);
+    }
+
 `;
