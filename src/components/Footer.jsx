bafe270133fe7c727aad2c58509bb411b9acd1ed
Christian Awelakoue <awelakoue@gmail.com>
2022-01-11 00:27:11 +0200

    add social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,41 @@
-import { Container } from "./styles/Container.styled";
-import { Flex } from "./styles/Flex.styled";
-import { StyledFooter } from "./styles/Footer.styled";
+import SocialIcons from './SocialIcons'
+import { Container } from './styles/Container.styled'
+import { Flex } from './styles/Flex.styled'
+import { StyledFooter } from './styles/Footer.styled'
 
 const Footer = () => {
     return (
         <StyledFooter>
             <Container>
-                <img src="./images/logo_white.svg" alt="" />
+                <img src='./images/logo_white.svg' alt='' />
 
                 <Flex>
                     <ul>
                         <li>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, illo?
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua
                         </li>
-                        <li>+27 00 1101 000</li>
-                        <li>exmple@huble.com</li>
+                        <li>+27-543-4567</li>
+                        <li>example@huddle.com</li>
                     </ul>
                     <ul>
                         <li>About Us</li>
                         <li>What We Do</li>
                         <li>FAQ</li>
                     </ul>
+
                     <ul>
                         <li>Career</li>
                         <li>Blog</li>
                         <li>Contact Us</li>
                     </ul>
-                    {/* Social Icons */}
+
+                    <SocialIcons />
                 </Flex>
-                <p>&copy; 2022 Hubble. All rights reserved</p>
+
+                <p>&copy; 2022 Huddle. All rights reserved</p>
             </Container>
         </StyledFooter>
-    );
+    )
 }
-
 export default Footer;
--- src/components/styles/Footer.styled.jsx
+++ src/components/styles/Footer.styled.jsx
@@ -1,30 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StyledFooter = styled.footer`
-    background-color: ${({ theme }) => theme.colors.footer};
-    color: #fff;
-    padding: 10px 0 60px;
-
+  background-color: ${({ theme }) => theme.colors.footer};
+  color: #fff;
+  padding: 100px 0 60px;
+  ul {
+    list-style-type: none;
+  }
+  ul li {
+    margin-bottom: 20px;
+  }
+  p {
+    text-align: right;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    text-align: center;
     ul {
-        list-style-type: none;
+      padding: 0;
     }
-
-    ul li {
-        margin-bottom: 20px;
-    }
-
     p {
-        text-align: right;
+      text-align: center;
     }
-
-    @media (max-width: ${({theme}) => theme.mobile}) {
-        text-align: center;
-        ul {
-            padding: 0;
-        }
-
-        p {
-            text-align: center;
-        }
-    }
-`;
+  }
+`
